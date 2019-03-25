351a93d83f4ae794793c30d400810d98d2f0b36c
Zaladdin <31769884+Zaladdin@users.noreply.github.com>
2019-03-25 23:25:05 +0300

    1

--- 23.11/index1.js
+++
@@ -1,4 +0,0 @@
-
-const fs = require("fs");
-const text = require('crypto').publicDecrypt(fs.readFileSync("key"), fs.readFileSync("secret"))
-console.log(text)
--- 23.11/index2.js
+++
@@ -1,9 +0,0 @@
-
-const crypto = require('crypto');
-const fs = require("fs");
-
-const file = fs.readFileSync("index1.js")
-const key = fs.readFileSync("key")
-
-const secret = crypto.publicEncrypt(key , file)
-fs.writeFileSync("secret2", secret)
